ba8abfa6c61a7967776f9a60296257871a99589e
Santiagope1234 <perezsantiago874@gmail.com>
2024-09-10 21:05:45 -0400

    cambios en tarea

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-let nombre = 12 || "Rafael"
-edad = 19 > 12
-nombre = 12
+let edad = prompt("Cual es tu edad?")
+
+if(edad >= 18) {
+    alert("Eres adulto")
+} else {
+    alert("No eres adulto")
+}

302e9a078e05d66d94a4313531005838c627e623
Santiagope1234 <perezsantiago874@gmail.com>
2024-10-09 17:07:14 -0400

    formulario

--- fase2/clase 1/index.js
+++ fase2/clase 1/index.js
@@ -1,24 +1,17 @@
-let frutas = ['Manzana', 'Peras', 'Frutas', 1]
-const nombres = [
-    {
-        nombre: 'Rafael'
-    },
-    {
-        nombre: 'Santiago'
-    },
-    {
-        nombre: 'Carla'
-    },
-    {
-        nombre: 'Fabiana'
-    },
-    {
-        nombre: 'Pedro'
-    },
+ // Objeto
+let persona = {
+    nombre: 'Santiago',
+    edad: 23,
+    correo: "perezsantiago874@gmail.com",
 
-]
+}
+
+console.log(persona) // es para motrar en consola, en casos reales usar return en funciones 
+
+function multiplicacion(n1, n2) {
+    console.log(n1 * n2)
+} 
+
+multiplicacion(2, 2)
 
 
-nombres.map((nombre) => {
-    console.log('Hola ' + nombre.nombre)
-})
